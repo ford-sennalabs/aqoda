26bfdb360b8856ebe60d6ffdfd32f8f6cf4f740c
pornsitbureenok <ford@sennalabs.com>
2021-01-05 17:00:58 +0700

    eiei good luck krub.

--- main.js
+++ main.js
@@ -7,19 +7,156 @@ class Command {
   }
 }
 
+class Hotel {
+
+  constructor(props) {
+    this.floors = props.floors;
+    this.roomPerFloors = props.amountOfRoomPerFloor;
+    const roomsAmout = props.amountOfRoomPerFloor * props.floors;
+    this.guess = [];
+    this.keycards = Array.from({ length: roomsAmout }).map((_, i) => {
+      return new KeyCard({keyCardNo:i});
+    });
+
+    this.rooms = Array.from({ length: roomsAmout }).map((_, i) => {
+      const floorNo = Math.floor(i/this.roomPerFloors)+1;
+      const roomNumber =  (i+1) % this.roomPerFloors === 0 ? this.roomPerFloors:(i+1) % this.roomPerFloors
+      return new Room({roomNo:floorNo.toString()+'0'+roomNumber});
+    });
+
+
+    console.log(
+      `Hotel created with ${props.floors} floor(s), ${this.roomPerFloors} room(s) per floor.`
+    )
+
+  }
+
+  findRoom(roomNo){
+  const foundRoomIndex = this.rooms.findIndex((room)=>room.roomNo == roomNo)
+    return this.rooms[foundRoomIndex]
+
+  }
+
+  findKeyCard(cardNo){
+    const selectedKeyCardIndex = this.keycards.findIndex((keyCard)=> keyCard.keyCardNo === cardNo);
+    return this.keycards[selectedKeyCardIndex];
+
+  }
+
+  roomBook(roomNo,name,age){
+    const findRoom = this.findRoom(roomNo);
+
+    if(findRoom.available){
+      const newGuess = new Guess({name,age})
+      const selectedKeyCardIndex = this.keycards.findIndex((keyCard)=>!keyCard.roomNo);
+      this.guess.push(newGuess);
+      findRoom.checkIn(newGuess, this.keycards[selectedKeyCardIndex].keyCardNo);
+      this.keycards[selectedKeyCardIndex].roomNo = findRoom.roomNo
+
+      console.log(
+          `Room ${findRoom.roomNo} is booked by ${newGuess.name} with keycard number ${this.keycards[selectedKeyCardIndex].keyCardNo}.`
+      )
+    }
+    else{
+      console.log(
+          `Cannot book room ${findRoom.roomNo} for ${name}, The room is currently booked by ${findRoom.guess.name}`
+      )
+
+    }
+
+  }
+
+  findAvailableRoom(){
+    const findRoomAvailable = this.rooms.filter((room)=>room.available).map((room)=>room.roomNo);
+    console.log(
+        `${findRoomAvailable}`
+    )
+
+  }
+
+  roomCheckOut(cardId,guessName){
+    const keyCard = this.findKeyCard(cardId);
+    const room = this.findRoom(keyCard.roomNo);
+
+    if(guessName === room.guess.name){
+      keyCard.roomNo = null;
+      room.checkOut()
+
+      console.log(`Room ${room.roomNo} is checkout.`);
+    }
+    else{
+      console.log(`Your name is not the same owner room (${room.roomNo})`);
+    }
+  }
+
+
+}
+
+class KeyCard {
+  constructor(props) {
+    this.roomNo = props.roomNo;
+    this.keyCardNo = props.keyCardNo;
+  }
+
+
+}
+
+class Room {
+  constructor(props) {
+    this.roomNo = props.roomNo;
+    this.available = true;
+    this.guess = null;
+    this.keycard = null;
+  }
+
+  checkIn(guess,keycardNo){
+    this.available = false;
+    this.guess = guess;
+    this.keycardNo = keycardNo;
+  }
+
+
+  checkOut(){
+    this.available = true;
+    this.guess = null;
+  }
+
+}
+
+class Guess{
+  constructor(props) {
+    this.name = props.name;
+    this.age = props.age;
+  }
+
+}
+
+
 function main() {
   const filename = 'input.txt'
   const commands = getCommandsFromFileName(filename)
+  let hotel;
 
   commands.forEach(command => {
     switch (command.name) {
       case 'create_hotel':
-        const [floor, roomPerFloor] = command.params
-        const hotel = { floor, roomPerFloor }
+        const [floors, amountOfRoomPerFloor] = command.params
+        hotel = new Hotel({floors, amountOfRoomPerFloor });
 
-        console.log(
-          `Hotel created with ${floor} floor(s), ${roomPerFloor} room(s) per floor.`
-        )
+        // console.log(
+        //   `Hotel created with ${floor} floor(s), ${roomPerFloor} room(s) per floor.`
+        // )
+        return
+      case 'book':
+        const [number, name,age] = command.params;
+        hotel.roomBook(number, name,age);
+        return
+      case 'list_available_rooms':
+        hotel.findAvailableRoom();
+        return
+      case 'checkout':
+        const [cardId, guessName] = command.params;
+        hotel.roomCheckOut(cardId, guessName);
         return
       default:
         return
