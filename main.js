c09e17cb0374b3ae030260758b0f9ec46bb65b1e
pornsitbureenok <ford@sennalabs.com>
2021-01-05 20:24:21 +0700

    eiei make it more finished..

--- main.js
+++ main.js
@@ -1,150 +1,11 @@
+
 const fs = require("fs");
+const Hotel = require("./model/Hotel");
 
 class Command {
   constructor(name, params) {
     this.name = name;
     this.params = params;
-  }
-}
-
-class Hotel {
-  constructor(props) {
-    this.floors = props.floors;
-    this.roomPerFloors = props.amountOfRoomPerFloor;
-    const roomsAmout = props.amountOfRoomPerFloor * props.floors;
-    this.guess = [];
-    this.keycards = Array.from({ length: roomsAmout }).map((_, i) => {
-      return new KeyCard({ keyCardNo: i + 1 });
-    });
-
-    this.rooms = Array.from({ length: roomsAmout }).map((_, i) => {
-      const floorNo = Math.floor(i / this.roomPerFloors) + 1;
-      const roomNumber =
-        (i + 1) % this.roomPerFloors === 0
-          ? this.roomPerFloors
-          : (i + 1) % this.roomPerFloors;
-      return new Room({ roomNo: floorNo.toString() + "0" + roomNumber });
-    });
-
-    console.log(
-      `Hotel created with ${props.floors} floor(s), ${this.roomPerFloors} room(s) per floor.`
-    );
-  }
-
-  isFull() {
-    return this.rooms.every((room) => !room.available);
-  }
-
-  findRoom(roomNo) {
-    const foundRoomIndex = this.rooms.findIndex(
-      (room) => room.roomNo == roomNo
-    );
-    return this.rooms[foundRoomIndex];
-  }
-
-  findKeyCard(cardNo) {
-    const selectedKeyCardIndex = this.keycards.findIndex(
-      (keyCard) => keyCard.keyCardNo === cardNo
-    );
-    return this.keycards[selectedKeyCardIndex];
-  }
-
-  listGuest() {
-    const guessNames = this.guess.map((person) => person.name);
-
-    console.log(guessNames.join(", "));
-  }
-
-  listGuestInRoom(roomNo) {
-    const guessNames = this.findRoom(roomNo);
-
-    console.log(guessNames && guessNames.guess.name);
-  }
-
-  roomBook(roomNo, name, age) {
-    const findRoom = this.findRoom(roomNo);
-
-    if (findRoom.available) {
-      const newGuess = new Guess({ name, age });
-      const selectedKeyCardIndex = this.keycards.findIndex(
-        (keyCard) => !keyCard.roomNo
-      );
-      this.guess.push(newGuess);
-      findRoom.checkIn(newGuess, this.keycards[selectedKeyCardIndex].keyCardNo);
-      this.keycards[selectedKeyCardIndex].roomNo = findRoom.roomNo;
-
-      console.log(
-        `Room ${findRoom.roomNo} is booked by ${newGuess.name} with keycard number ${this.keycards[selectedKeyCardIndex].keyCardNo}.`
-      );
-    } else {
-      if (!this.isFull()) {
-        console.log(
-          `Cannot book room ${findRoom.roomNo} for ${name}, The room is currently booked by ${findRoom.guess.name}`
-        );
-      } else {
-        console.log("Hotel is fully booked.");
-      }
-    }
-  }
-
-  findAvailableRoom() {
-    const findRoomAvailable = this.rooms
-      .filter((room) => room.available)
-      .map((room) => room.roomNo);
-    console.log(`${findRoomAvailable}`);
-  }
-
-  roomCheckOut(cardId, guessName) {
-    const keyCard = this.findKeyCard(cardId);
-    const room = this.findRoom(keyCard.roomNo);
-
-    if (guessName === room.guess.name) {
-      room.checkOut();
-      const foundGuessIndex = this.guess.findIndex((guess) => {
-        return guess.roomNo === keyCard.roomNo;
-      });
-      this.guess.splice(foundGuessIndex, 1);
-      keyCard.roomNo = null;
-      console.log(`Room ${room.roomNo} is checkout.`);
-    } else {
-      console.log(
-        `Only ${room.guess.name} can checkout with keycard number ${room.keycardNo}.`
-      );
-    }
-  }
-}
-
-class KeyCard {
-  constructor(props) {
-    this.roomNo = props.roomNo;
-    this.keyCardNo = props.keyCardNo;
-  }
-}
-
-class Room {
-  constructor(props) {
-    this.roomNo = props.roomNo;
-    this.available = true;
-    this.guess = null;
-    this.keycardNo = null;
-  }
-
-  checkIn(guess, keycardNo) {
-    this.available = false;
-    this.guess = guess;
-    this.keycardNo = keycardNo;
-  }
-
-  checkOut() {
-    this.available = true;
-    this.guess = null;
-  }
-}
-
-class Guess {
-  constructor(props) {
-    this.name = props.name;
-    this.age = props.age;
   }
 }
 
@@ -180,6 +41,22 @@ function main() {
       case "get_guest_in_room":
         const [roomNo] = command.params;
         hotel.listGuestInRoom(roomNo);
+        return;
+      case "list_guest_by_age":
+        const [operator,ageRange] = command.params;
+        hotel.listGuestByAge(operator,ageRange);
+        return;
+      case "list_guest_by_floor":
+        const [fl] = command.params;
+        hotel.listGuestByFloor(fl);
+        return;
+      case "checkout_guest_by_floor":
+        const [checkInfl] = command.params;
+        hotel.checkoutGuestByFloor(checkInfl);
+        return;
+      case "book_by_floor":
+        const [bookFl,bookerName,bookerAge] = command.params;
+        hotel.bookByFloor(bookFl,bookerName,bookerAge);
         return;
       default:
         return;
